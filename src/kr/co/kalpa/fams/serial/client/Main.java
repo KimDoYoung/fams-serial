a7664f366d1d86d48a0a9ec75d7bd90aed95bf68
KimDoYoung <kdy987@gmail.com>
2020-07-08 17:24:58 +0900

    commit src

---
+++ src/kr/co/kalpa/fams/serial/client/Main.java
@@ -0,0 +1,7 @@
+package kr.co.kalpa.fams.serial.client;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("FAMS Serial client running...");
+	}
+}
---
+++ src/kr/co/kalpa/fams/serial/server/Main.java
@@ -0,0 +1,7 @@
+package kr.co.kalpa.fams.serial.server;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("FAMS Serial running");
+	}
+}
